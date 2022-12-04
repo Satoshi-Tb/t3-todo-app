2ad8a9ca6f1f4f259f675d65855bcd7f5f0fcf6e
Satoshi-Tb <sulp91_fy@yahoo.co.jp>
2022-12-04 11:45:15 +0900

    サーバーサイドにTODO CRUD処理追加

---
+++ src/server/trpc/router/todo.ts
@@ -0,0 +1,64 @@
+import {
+  createTaskSchema,
+  getSingleTaskSchema,
+  updateTaskSchema,
+  deleteTaskSchema,
+} from "../../../schema/todo";
+
+import { router, procedure, protectedProcedure } from "../trpc";
+
+export const todoRouter = router({
+  // validationを行う場合、inputで指定
+  // 更新の場合、mutationを指定
+  createTask: protectedProcedure
+    .input(createTaskSchema)
+    .mutation(async ({ ctx, input }) => {
+      const task = await ctx.prisma.task.create({
+        data: {
+          ...input,
+          user: {
+            connect: {
+              id: ctx.session?.user?.id,
+            },
+          },
+        },
+      });
+      return task;
+    }),
+
+  getTasks: procedure.query(({ ctx }) => {
+    return ctx.prisma.task.findMany({
+      where: {
+        userId: ctx.session?.user?.id,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  }),
+
+  getSingleTask: protectedProcedure
+    .input(getSingleTaskSchema)
+    .query(({ ctx, input }) => {
+      return ctx.prisma.task.findUnique({
+        where: {
+          id: input.taskId,
+        },
+      });
+    }),
+
+  updateTask: protectedProcedure
+    .input(updateTaskSchema)
+    .mutation(async ({ ctx, input }) => {
+      const task = await ctx.prisma.task.update({
+        where: {
+          id: input.taskId,
+        },
+        data: {
+          title: input.title,
+          body: input.body,
+        },
+      });
+      return task;
+    }),
+});
